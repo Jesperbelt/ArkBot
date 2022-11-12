426ec53d270e19247031fb2402a070a0e50e3190
Jesper <jesper_beltman@hotmail.com>
2022-11-12 22:51:28 +0100

    Commit

--- DoD/Table/Data_bank.cs
+++ DoD/Table/Data_bank.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
-namespace DoD
+namespace Table
 {
     public class Data_bank
     {
